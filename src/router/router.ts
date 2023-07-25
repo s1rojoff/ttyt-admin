0949fae20ace0e76adb775fa4cd2e876de26271c
Sirojov Dilshod <87010216+SirojovDev@users.noreply.github.com>
2023-07-25 11:22:49 +0500

    project is prepared to working

---
+++ src/router/router.ts
@@ -0,0 +1,7 @@
+export const routers = [
+  {
+    path: '/',
+    name: 'Dashboard',
+    component: () => import('@/views/dashboard/index.vue')
+  }
+]
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1 class="text-red-500">hello world</h1>
+    </div>
+</template>
