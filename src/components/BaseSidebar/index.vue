6cfb2b872fc29efda50dce339240285ea4ece3c9
Sirojov Dilshod <87010216+SirojovDev@users.noreply.github.com>
2023-08-10 10:37:04 +0500

    active class added to subitem

--- src/components/BaseSidebar/index.vue
+++ src/components/BaseSidebar/index.vue
@@ -3,6 +3,8 @@ import { type PropType } from 'vue'
 import BaseIcon from '@/components/BaseIcon/index.vue'
 import type { Items } from '@/interfaces/index'
 import { useSidebarStore } from '@/stores/index'
+import { useRoute, type RouteLocationNormalized } from 'vue-router';
+const $route: RouteLocationNormalized = useRoute();
 const store = useSidebarStore()
 const props = defineProps({
   arrItems: {
@@ -17,6 +19,10 @@ function handeItemClicked(id: number): void {
     store.$state.accordionItem = 123
   }
 }
+const isExactActive = (route: string): boolean => {
+  return $route.path === route;
+};
+
 </script>
 <template>
   <div class="h-[100dvh] bg-[#001018] p-5 pr-0">
@@ -30,8 +36,8 @@ function handeItemClicked(id: number): void {
         :key="index"
         class="text-white pb-3 cursor-pointer"
       >
-        <div class="flex items-center justify-start">
-          <p class="text-xl font-normal" @click.stop="handeItemClicked(item.id)">{{ item.name }}</p>
+        <div class="flex items-center justify-start" @click.stop="handeItemClicked(item.id)">
+          <p class="text-xl font-normal">{{ item.name }}</p>
           <BaseIcon
             :class="[store.$state.accordionItem === item.id ? 'rotate-180' : '']"
             class="w-3 h-3 text-white ml-3"
@@ -41,9 +47,11 @@ function handeItemClicked(id: number): void {
         <div class="ml-5" v-if="store.$state.accordionItem === item.id">
           <router-link
             class="sub-item text-base py-1 block hover:ml-2"
+            v-slot="{ isExactActive }"
             v-for="(subItem, subIndex) in item.subItem"
             :key="subIndex"
             :to="subItem.route"
+            :class="{'text-red-500': isExactActive(subItem.route)}"
           >
             {{ subItem.name }}
           </router-link>
