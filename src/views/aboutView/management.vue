e697d13fe6c01e21e8275ef9dd0e58905c7d85fb
unknown <you@example.comhusniddinxomidov2002@gmail.com>
2023-08-10 11:10:55 +0500

    added BaseInput

--- src/views/aboutView/management.vue
+++ src/views/aboutView/management.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import BaseInput from '@/components/BaseInput/index.vue'
+</script>
 <template>
   <div>rahbariyat page</div>
+  <BaseInput placeholder="something"/>
 </template>
