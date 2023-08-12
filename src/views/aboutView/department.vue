4d228a48e3a89efc5709bb4fbb3ae09c59a2b6e7
Sirojov Dilshod <87010216+SirojovDev@users.noreply.github.com>
2023-08-12 09:15:31 +0500

    img componenent created

--- src/views/aboutView/department.vue
+++ src/views/aboutView/department.vue
@@ -1,4 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+const imageSrc = ref<string>('')
+const inputFile = ref<string | null>('')
+function test() {
+  console.log(inputFile)
+}
+function handleFileChange(event: any) {
+  const file = event.target.files[0]
+  if (file) {
+    const reader = new FileReader()
+
+    reader.onload = (e: any) => {
+      imageSrc.value = e.target.result
+    }
+
+    reader.readAsDataURL(file)
+  }
+}
+</script>
 <template>
-  <div>kafedra page</div>
+  <div>
+    <input ref="inputFile" type="file" @change="handleFileChange" />
+    <div class="w-40 h-40 rounded-lg border border-red-600">
+      <p><img :src="imageSrc" alt="" /></p>
+    </div>
+    <button @click="test">click me</button>
+  </div>
 </template>
