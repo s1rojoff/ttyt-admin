e51b04f7970320943d39d024a81c9594f868f9f4
unknown <you@example.comhusniddinxomidov2002@gmail.com>
2023-08-12 09:48:54 +0500

    added components

--- src/views/aboutView/management.vue
+++ src/views/aboutView/management.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-  import BaseInput from '@/components/BaseInput/index.vue'
-  import BaseButton from '@/components/BaseButton/index.vue'
-  import BaseTextaera from '@/components/BaseTextarea/index.vue'
 </script>
 <template>
   <div class="text-center uppercase text-xl">
